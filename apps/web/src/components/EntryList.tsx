4bbdcf5bd995b8e51e4b1e7ff21078d7d346237c
Maarten Louage <me@maartenlouage.nl>
2025-05-03 19:05:44 +0200

    (chore) ui changes for release details page

--- apps/web/src/components/EntryList.tsx
+++ apps/web/src/components/EntryList.tsx
@@ -26,11 +26,24 @@ interface EntryListProps {
 
 const cmsUrl = import.meta.env.VITE_CMS_URL || 'http://localhost:1337';
 
+const models = {
+  page: 'text-green-700 bg-green-50 ring-green-600/20',
+  author: 'text-blue-800 bg-blue-50 ring-blue-600/20',
+  tag: 'text-orange-800 bg-orange-50 ring-orange-600/20',
+  article: 'text-purple-800 bg-purple-50 ring-purple-600/20',
+  homepage: 'text-yellow-800 bg-yellow-50 ring-yellow-600/20',
+  'global-setting': 'text-pink-800 bg-pink-50 ring-pink-600/20',
+}
+
 export default function EntryList({ events = [] }: EntryListProps) {
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     return date.toLocaleDateString('nl-NL', { year: 'numeric', month: 'long', day: 'numeric' });
   };
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
 
   return (
       <ul role="list" className="divide-y divide-gray-100">
@@ -38,7 +51,16 @@ export default function EntryList({ events = [] }: EntryListProps) {
             <li key={event.id} className="flex items-center justify-between gap-x-6 py-5">
               <div className="min-w-0">
                 <div className="flex items-start gap-x-3">
-                  <p className="text-sm/6 font-semibold text-gray-900">{event.model} {event.entryTitle ?? event.documentId}</p>
+
+                  <p
+                      className={classNames(
+                          models[event.model],
+                          'mt-0.5 rounded-md px-1.5 py-0.5 text-xs font-medium whitespace-nowrap ring-1 ring-inset',
+                      )}
+                  >
+                    {event.model}
+                  </p>
+                  <p className="text-sm/6 font-semibold text-gray-900">{event.entryTitle ?? event.documentId}</p>
                 </div>
                 <div className="mt-1 flex items-center gap-x-2 text-xs/5 text-gray-500">
                   <p className="whitespace-nowrap">
